739e0c789abcfc02d2f6571bb9fa70624f2c0a19
PROMETHIA <electriccobras@gmail.com>
2023-05-12 08:56:53 -0400

    Cleaned up simple example

--- examples/simple.rs
+++ examples/simple.rs
@@ -27,41 +27,8 @@ fn setup(mut c: Commands, q: Query<Entity>) {
     c.entity(window).insert((
         List(vec![1, 2, 3]),
         Map(HashMap::from_iter([
-            ("bruh".into(), 12),
+            ("test".into(), 12),
             ("two".into(), 24),
         ])),
     ));
 }
-
-// fn setup(mut editors: ResMut<ValueEditors>) {
-//     // editors.editors.insert(
-//     //     std::any::type_name::<Window>().to_string(),
-//     //     Box::new(|ui, value, _| {
-//     //         ui.horizontal(|ui| {
-//     //             ui.label("title: ");
-//     //             let value = match value.reflect_mut() {
-//     //                 bevy::reflect::ReflectMut::Struct(val) => val,
-//     //                 _ => panic!(),
-//     //             };
-//     //             let title = value.get_field_mut::<String>("title").unwrap();
-
-//     //             ui.text_edit_singleline(title);
-//     //         });
-//     //     }),
-//     // );
-
-//     // editors.applicators.insert(
-//     //     std::any::type_name::<Window>().to_string(),
-//     //     Box::new(|entity, value| {
-//     //         let refl_comp = entity.world_scope(|world| {
-//     //             world
-//     //                 .resource::<AppTypeRegistry>()
-//     //                 .read()
-//     //                 .get_type_data::<ReflectComponent>(TypeId::of::<Window>())
-//     //                 .unwrap()
-//     //                 .clone()
-//     //         });
-//     //         refl_comp.apply(entity, &*value);
-//     //     }),
-//     // );
-// }
